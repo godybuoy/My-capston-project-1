b2d71eb8f7fd06f70bf5736e1adaeab346e8279d
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-04-26 01:06:54 +0500

    Make changes to javascript and css

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const speakersCountry = document.querySelectorAll('.speakers-country');
 const speakersDescription = document.querySelectorAll('.speakers-description');
 const speakersBgImage = document.querySelectorAll('.img-transparent-bkgrnd');
 const speakersImage = document.querySelectorAll('.speakers-images');
-
+const morebtn = document.querySelector('#moreBtn');
+const speakercontainerfordesktop = document.querySelectorAll('.speakers-info-container ul')
+console.log(speakercontainerfordesktop)
+morebtn.addEventListener('click',()=>{
+  speakercontainerfordesktop.forEach((item)=>{
+    item.classList.remove('display-none')
+  })
+  
+})
 const speakersInfo = [
   {
     name: 'John Duke',
@@ -57,16 +65,22 @@ for (let i = 0; i < speakersName.length; i += 1) {
   speakersImage[i].src = speakersInfo[i].image;
 }
 
-const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenu = document.querySelector('.mobile');
+mobileMenu.addEventListener('click',()=>{
+  document.querySelector('.intro-part').style.display = 'none'
+  mobileMenu.classList.add('mobile-menu-appear');
+})
 
-document.querySelector('intro-part').addEventListener('click', (e) => {
-  if (e.target.classList[0] === 'mobile' || e.target.classList[0] === 'lines') {
-    mobileMenu.classList.add('mobile-menu-appear');
-  }
-});
+// document.querySelector('.intro-part').addEventListener('click', (e) => {
+//   if (e.target.classList[0] === 'mobile' || e.target.classList[0] === 'lines') {
+//     mobileMenu.classList.add('mobile-menu-appear');
+//   }
+// });
 
-mobileMenu.addEventListener('click', (e) => {
-  if (e.target.classList[0] === 'mobile-menu-disappear-btn') {
-    mobileMenu.classList.remove('mobile-menu-appear');
-  }
-});
+// mobileMenu.addEventListener('click', (e) => {
+//   if (e.target.classList[0] === 'mobile-menu-disappear-btn') {
+//     mobileMenu.classList.remove('mobile-menu-appear');
+//   }
+// });
+
+
